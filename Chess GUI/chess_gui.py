1b44bd879ff1eb60db7cb6e7e42e900557404e94
dstiven09 <stivendf@gmail.com>
2024-08-04 16:54:16 +0200

    Update chess_gui.py

--- Chess GUI/chess_gui.py
+++ Chess GUI/chess_gui.py
@@ -4,9 +4,11 @@ from config import STOCKFISH_PATH
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/analyze_fen', methods=['POST'])
 def analyze_fen_endpoint():
@@ -16,6 +18,7 @@ def analyze_fen_endpoint():
     moves = int(data['moves'])
     result = analyze_fen(fen, STOCKFISH_PATH, top_n=moves, depth=depth)
     return jsonify({'result': result})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
